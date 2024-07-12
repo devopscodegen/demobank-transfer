074806e7be09a7946909cdd9291ea65a1d0ab6d9
Sagar Velankar <Sagar Velankar>
2024-07-12 22:47:14 +0530

    fix: Domain driven design

--- src/main/java/com/demobank/transfer/port/adapter/controller/TransferController.java
+++ src/main/java/com/demobank/transfer/port/adapter/controller/TransferController.java
@@ -6,16 +6,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demobank.transfer.port.adapter.service.TransferService;
+import com.demobank.transfer.application.TransferApplicationService;
+import com.demobank.transfer.application.TransferCommand;
 
 @RestController
 @RequestMapping("/transfer")
 public class TransferController {
     @Autowired
-    private TransferService transferService;
+    private TransferApplicationService transferApplicationService;
 
     @PostMapping
     public TransferResponse transfer(@RequestBody TransferRequest request) {
-        return transferService.transfer(request.getFromAccountId(), request.getToAccountId(), request.getAmount(), request.getCurrency());
+        return new TransferResponse(transferApplicationService.transfer(new TransferCommand(request.getFromAccountId(), request.getToAccountId(), request.getAmount(), request.getCurrency())));
     }
 }
